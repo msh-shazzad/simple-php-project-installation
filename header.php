e5753539328e222e3dfe3128762500d6dc70fde5
msh-shazzad <shazzadhossen.sh@gmail.com>
2017-03-25 15:20:25 +0600

    contains necessary header html files

---
+++ header.php
@@ -0,0 +1,11 @@
+// 'header.php' file contains the necessary html codes
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
+<link rel="stylesheet" href="css/style.css" type="text/css"  />
+</head>
